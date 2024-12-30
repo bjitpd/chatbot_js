ee898f8ea0f2bfdd3c4231e839ea2fdc658e9f64
Israr Munim Chowdhury <israr.munim@bjitgroup.com>
2024-12-30 15:08:17 +0600

    ch1

--- scrOne.js
+++ scrOne.js
@@ -45,10 +45,11 @@
     iframe.style.left = "20px";
   }
   window.addEventListener("message", (event) => {
+    console.log(event, "TRIGGERED");
+
     if (event.origin !== "http://localhost:3090") {
       return; // This line might be the cause if origins do not match.
     }
-
     if (event.data.status == "active") {
       iframe.style.height = chatbotConfig?.height;
       iframe.style.width = chatbotConfig?.width;
