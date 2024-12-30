9a7e0a3ef363727898c5f7c72d8eabd6679b0ea2
Israr Munim Chowdhury <israr.munim@bjitgroup.com>
2024-12-30 15:11:37 +0600

    ch3

--- scrTwo.js
+++ scrTwo.js
@@ -45,11 +45,13 @@
     iframe.style.left = "20px";
   }
   window.addEventListener("message", (event) => {
-    console.log(event, "TRIGGERED");
-
     if (event.origin !== "http://localhost:3090") {
+      console.log("NOT VALID");
+
       return; // This line might be the cause if origins do not match.
     }
+    console.log(event, "TRIGGERED");
+
     if (event.data.status == "active") {
       iframe.style.height = chatbotConfig?.height;
       iframe.style.width = chatbotConfig?.width;
