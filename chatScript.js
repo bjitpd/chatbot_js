2054b73163b92d49880d3e02c221c1ea4c507bc7
Israr Munim Chowdhury <israr.munim@bjitgroup.com>
2024-12-31 09:08:46 +0600

    nf1

---
+++ chatScript.js
@@ -0,0 +1,67 @@
+(function () {
+  if (window.ChatbotScriptLoaded) return;
+  window.ChatbotScriptLoaded = true;
+
+  const currentScriptTag =
+    document.currentScript ||
+    Array.from(document.getElementsByTagName("script")).pop();
+
+  const paramObj = JSON.parse(currentScriptTag.getAttribute("data-params"));
+
+  console.log(paramObj, "OBJECT");
+  const chatbotConfig = {
+    botUrl: "https://ainexus-staging.bjitgroup.com/chatapp",
+    width: "90px",
+    height: "90px",
+    position: "bottom-right",
+    queryParams: {
+      bgColor: paramObj?.bgColor,
+      headerTitle: paramObj?.headerTitle || "Chat with us",
+      userId: paramObj?.userId,
+      apiKey: paramObj?.apiKey,
+      assistantId: paramObj?.assistantId,
+      screenWidth: paramObj?.screenWidth,
+      webUrl: paramObj?.webUrl,
+    },
+  };
+
+  const queryString = new URLSearchParams(chatbotConfig.queryParams).toString();
+  const chatbotUrlWithParams = `${chatbotConfig.botUrl}?${queryString}`;
+
+  const iframe = document.createElement("iframe");
+  iframe.src = chatbotUrlWithParams;
+  iframe.style.width = chatbotConfig.width;
+  iframe.style.height = chatbotConfig.height;
+  iframe.style.position = "fixed";
+  iframe.style.border = "none";
+  iframe.style.zIndex = "9999";
+  iframe.style.borderRadius = "18px";
+  iframe.style.allowTransparency = "true";
+
+  if (chatbotConfig.position === "bottom-right") {
+    iframe.style.bottom = "0";
+    iframe.style.right = "20px";
+  } else if (chatbotConfig.position === "bottom-left") {
+    iframe.style.bottom = "0";
+    iframe.style.left = "20px";
+  }
+  window.addEventListener("message", (event) => {
+    console.log(event, "TRIGGERED");
+
+    if (event.origin !== "https://ainexus-staging.bjitgroup.com") {
+      console.log("NOT VALID");
+
+      return; // This line might be the cause if origins do not match.
+    }
+
+    if (event.data.status == "active") {
+      iframe.style.height = chatbotConfig?.height;
+      iframe.style.width = chatbotConfig?.width;
+    } else if (event.data.status == "inactive") {
+      iframe.style.height = "90px";
+      iframe.style.width = "90px";
+    }
+  });
+
+  document.body.appendChild(iframe);
+})();
